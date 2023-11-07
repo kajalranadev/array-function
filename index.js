ae144e007a53e0d2ea6d0f74e7da5b6054cf6453
Kajal Rana <114815118+kajalranadev@users.noreply.github.com>
2023-11-07 18:01:48 +0000

    make a array function

---
+++ index.js
@@ -0,0 +1,8 @@
+const myArray = [1, 2, 3, 4, 5, 6];
+function getFifthElement(arr) {
+  const fifthElement = arr[4];
+  return fifthElement;
+}
+
+const number = getFifthElement(myArray);
+console.log(number);
